83cf1a1d610632311ebd6ac4914939282b3f3db2
Crazy <143905862+Crazybloy2@users.noreply.github.com>
2025-01-11 02:01:27 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,58 +1,7 @@
-WIKIDOT.modules.MembershipApplyModule = {};
-WIKIDOT.modules.MembershipApplyModule.listeners = {
-  apply: function(b) {
-    const a = document.getElementById("age").value;
-    const c = document.getElementById("code").value;
+const a = document.getElementById("age").value;
+const c = document.getElementById("code").value;
+const apply = "Age: " + a + " years" + "\n" + "Code: " + c;
+document.getElementById("membership-by-apply-text").value = apply;
 
-    if (!a || !c) {
-      document.getElementById("SR").innerHTML = "Age or Code is missing";
-      document.getElementById("SR").style.color = "red";
-      return;
-    }
 
-    const applyText = "Age: " + a + " years" + "\n" + "Code: " + c;
-
-    document.getElementById("applyText").value = applyText;
-    document.getElementById("apply").value = applyText;
-
-    var formData = OZONE.utils.formToArray("membership-by-apply-form");
-    formData.push({ name: 'age', value: "Age: " + a + " years" });
-    formData.push({ name: 'code', value: "Code: " + c });
-
-    formData.action = "MembershipApplyAction";
-    formData.event = "apply";
-
-    OZONE.ajax.requestModule("membership/MembershipApplySuccessModule", formData, WIKIDOT.modules.MembershipApplyModule.callbacks.apply);
-  }
-};
-
-WIKIDOT.modules.MembershipApplyModule.callbacks = {
-  apply: function(b) {
-    if (!WIKIDOT.utils.handleError(b)) {
-      return;
-    }
-
-    var a = new OZONE.dialogs.SuccessDialog();
-    a.content = "Your application has been sent and now awaits to be processed by the site administrators.";
-    a.addButtonListener("close message", function() {
-      window.location.reload();
-    });
-    a.show();
-
-    document.getElementById("SR").innerHTML = "Application sent.";
-    document.getElementById("SR").style.color = "green";
-    document.getElementById("age").value = '';
-    document.getElementById("code").value = '';
-    document.getElementById("applyText").value = '';
-  }
-};
-
-WIKIDOT.modules.MembershipApplyModule.init = function() {
-  OZONE.dom.onDomReady(function() {
-    if ($("membership-by-apply-text")) {
-      YAHOO.util.Event.addListener("mba-apply", "click", WIKIDOT.modules.MembershipApplyModule.listeners.apply);
-    }
-  }, "dummy-ondomready-block");
-};
-
-WIKIDOT.modules.MembershipApplyModule.init();
+WIKIDOT.modules.MembershipApplyModule={};WIKIDOT.modules.MembershipApplyModule.listeners={apply:function(b){var a=OZONE.utils.formToArray("membership-by-apply-form");a.action="MembershipApplyAction";a.event="apply";OZONE.ajax.requestModule("membership/MembershipApplySuccessModule",a,WIKIDOT.modules.MembershipApplyModule.callbacks.apply)}};WIKIDOT.modules.MembershipApplyModule.callbacks={apply:function(b){if(!WIKIDOT.utils.handleError(b)){return}var a=new OZONE.dialogs.SuccessDialog();a.content="Your application has been sent and now awaits to be processed by the site administrators.";a.addButtonListener("close message",function(){window.location.reload()});a.show()}};WIKIDOT.modules.MembershipApplyModule.init=function(){OZONE.dom.onDomReady(function(){if($("membership-by-apply-text")){YAHOO.util.Event.addListener("mba-apply","click",WIKIDOT.modules.MembershipApplyModule.listeners.apply);var a=new OZONE.forms.lengthLimiter("membership-by-apply-text","membership-by-apply-text-left",200)}},"dummy-ondomready-block")};WIKIDOT.modules.MembershipApplyModule.init();
