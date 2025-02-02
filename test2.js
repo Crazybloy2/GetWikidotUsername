235c0421052d4e12852aa96a7afef20db2a33cbd
Crazy <143905862+Crazybloy2@users.noreply.github.com>
2025-02-02 05:30:01 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,15 +1,32 @@
 
+  
+const elOne = document.createElement('input');
+elOne.setAttribute('id', 'age');
+elOne.setAttribute('placeholder', 'age');
+const elTwo = document.createElement('input');
+elTwo.setAttribute('id', 'code');
+elTwo.setAttribute('placeholder', 'code');
+const elThree = document.createElement('div');
+elThree.setAttribute('id', 'SR')
+
+document.getElementById('membership-apply-box').append(elOne);
+
+document.getElementById('membership-apply-box').append(elTwo);
+
+document.getElementById('membership-apply-box').append(elThree);
+
+
+setTimeout(() => {
   const a = document.getElementById("age").value;
   const c = document.getElementById("code").value;
+  const apply = "Age: " + a + " years" + "\n" + "Code: " + c;
+  document.getElementById("membership-by-apply-text").value = apply;
+}
 
   if (!a || !c) {
     document.getElementById("SR").innerHTML = "Code or Age is missing";
     document.getElementById("SR").style.color = "red";
-    return;
-  }
-
-  const apply = "Age: " + a + " years" + "\n" + "Code: " + c;
-  document.getElementById("hello").value = apply;
+}
 
 
 
