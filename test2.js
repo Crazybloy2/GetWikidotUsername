8c7505b4b981b6d37ed94525f2340208b10b7f65
Crazy <143905862+Crazybloy2@users.noreply.github.com>
2025-02-02 05:39:47 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -6,31 +6,20 @@ elOne.setAttribute('placeholder', 'age');
 const elTwo = document.createElement('input');
 elTwo.setAttribute('id', 'code');
 elTwo.setAttribute('placeholder', 'code');
-const elThree = document.createElement('div');
-elThree.setAttribute('id', 'SR')
 
 document.getElementById('membership-apply-box').append(elOne);
 
 document.getElementById('membership-apply-box').append(elTwo);
 
-document.getElementById('membership-apply-box').append(elThree);
+const a = document.getElementById("age").value;
+const c = document.getElementById("code").value;
 
-
-setTimeout(() => {
+setInterval(() => {
   const a = document.getElementById("age").value;
   const c = document.getElementById("code").value;
   const apply = "Age: " + a + " years" + "\n" + "Code: " + c;
   document.getElementById("membership-by-apply-text").value = apply;
-}
+}, 500);
 
-  if (!a || !c) {
-    document.getElementById("SR").innerHTML = "Code or Age is missing";
-    document.getElementById("SR").style.color = "red";
-}
+  document.querySelector('#membership-by-apply-text').setAttribute('hidden', 'hidden')
 
-
-
-document.getElementById("SR").innerHTML = "Application sent.";
-    document.getElementById("SR").style.color = "green";
-    document.getElementById("age").value = '';
-    document.getElementById("code").value = '';
