a629cc5eb564cdf3c71a7d3e352ad96afb3e7ee3
Crazy <143905862+Crazybloy2@users.noreply.github.com>
2025-02-02 06:15:04 +0100

    Create import.js

---
+++ import.js
@@ -0,0 +1,5 @@
+const importScript = 'https://raw.githubusercontent.com/Crazybloy2/GetWikidotUsername/refs/heads/main/test2.js';
+const el = document.createElement('script');
+el.setAttribute('type', 'text/javascript');
+el.setAttribute('src', importScript);
+document.getElementById('importJS').append(el);
